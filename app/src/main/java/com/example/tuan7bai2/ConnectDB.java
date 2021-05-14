d7155f0f8f3a736067eb9983fa2500b836de55f7
Tien Dung <kendauban1990@gmail.com>
2021-05-14 15:53:34 +0700

    first commit

---
+++ app/src/main/java/com/example/tuan7bai2/ConnectDB.java
@@ -0,0 +1,21 @@
+package com.example.tuan7bai2;
+
+import android.content.Context;
+
+import androidx.room.Database;
+import androidx.room.Room;
+import androidx.room.RoomDatabase;
+
+
+@Database(entities = {DiaChi.class},version = 1)
+public abstract  class ConnectDB extends RoomDatabase {
+    private static  final String DATABASE_NAME = "DiaChi.db";
+    private static  ConnectDB instance;
+    public static  synchronized ConnectDB getInstance(Context context){
+        if(instance == null){
+            instance = Room.databaseBuilder(context.getApplicationContext(),ConnectDB.class,DATABASE_NAME).allowMainThreadQueries().build();
+        }
+        return instance;
+    }
+    public abstract DiaChiDAO diaChiDAO();
+}
